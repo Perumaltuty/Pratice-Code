6d756c617912ab229ed2f732ae155f404e76485d
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2022-09-19 16:36:09 +0530

    Create 4bit latch inverted gate_asynchronous preset.v

---
+++ 4bit latch inverted gate_asynchronous preset.v
@@ -0,0 +1,13 @@
+module latch (g, d, pre, q);
+        input        g, pre;
+        input  [3:0] d;
+        output [3:0] q;
+        reg    [3:0] q;
+        always @(g or d or pre)
+        begin
+           if (pre)
+              q <= 4’b1111;
+           else if (~g)
+              q <= d;
+        end
+        endmodule
