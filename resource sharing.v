fa1635aeee901ef02b88a5cee6137bb9df024ddc
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2022-10-22 10:11:43 +0530

    Create resource sharing.v

---
+++ resource sharing.v
@@ -0,0 +1,15 @@
+module addsub(a, b, c, oper, res);
+	input        oper;
+	input  [7:0] a;
+	input  [7:0] b;
+	input  [7:0] c;
+	output [7:0] res;
+	reg    [7:0] res;
+	always @(a or b or c or oper)
+	begin
+	   if (oper == 1’b0)
+	      res = a + b;
+	   else
+	      res = a - c;
+	end
+        endmodule
